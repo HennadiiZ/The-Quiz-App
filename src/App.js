94283ffbdd74e4fdd2e1ebeb5b7509f5f16578e0
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-19 15:46:50 -0600

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useEffect, useReducer } from 'react';
 // import DateCounter from './components/DateCounter';
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  questions: [],
+  // isLoading state
+  status: 'loading', //state
+  // states: 'loading', 'error', 'ready', 'active', 'finished' - it has nothing to do with useReducer
+};
+
+function reducer(state, action) {
+  // switch (action.type) {
+  // }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className='App'>
       {/* <DateCounter /> */}
@@ -17,3 +39,7 @@ function App() {
 }
 
 export default App;
+
+// npm start
+// npm run server
+// http://localhost:8000/questions
